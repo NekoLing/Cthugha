212bb0e04dd6b851dd47be4cfd4db93fe2d55d4b
“NekoLing” <gaoqi135@icloud.com>
2021-12-21 00:14:14 +0800

    导入一些工具类

---
+++ src/main/java/com/cthugha/util/CookieUtils.java
@@ -0,0 +1,52 @@
+package com.cthugha.util;
+
+import org.springframework.http.HttpHeaders;
+
+import javax.servlet.ServletRequest;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @Classname CookieUtils
+ * @Description 处理cookie信息的工具类
+ * @Date 2021/12/9 20:37
+ * @Created by gaoqi
+ */
+public class CookieUtils {
+
+    /**
+     * 从请求体中获取token，默认token储存在cookie中
+     * @param servletRequest 请求体
+     * @return token
+     */
+    public static String getToken(ServletRequest servletRequest) {
+        Cookie[] cookies = ((HttpServletRequest) servletRequest).getCookies();
+        String token = "";
+        if (cookies != null) {
+            for (int i = 0; i < cookies.length; i++) {
+                if (cookies[i].getName().equals("token"))
+                    token = cookies[i].getValue();
+            }
+        }
+        return token;
+    }
+
+    /**
+     * 根据参数名从cookie中获取数据
+     * @param servletRequest 请求体
+     * @param name 参数名
+     * @return
+     */
+    public static String get(ServletRequest servletRequest, String name) {
+        Cookie[] cookies = ((HttpServletRequest) servletRequest).getCookies();
+        String token = "";
+        if (cookies != null) {
+            for (int i = 0; i < cookies.length; i++) {
+                if (cookies[i].getName().equals(name))
+                    token = cookies[i].getValue();
+            }
+        }
+        return token;
+    }
+
+}
