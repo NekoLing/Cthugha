8e101aed32d76c24065c6872123590e06efdeda1
“NekoLing” <gaoqi135@icloud.com>
2021-12-23 15:47:02 +0800

    修复一些小bug

--- src/main/java/com/cthugha/CthughaApplication.java
+++ src/main/java/com/cthugha/CthughaApplication.java
@@ -2,7 +2,9 @@ package com.cthugha;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
+@ServletComponentScan("com/cthugha/filter")
 @SpringBootApplication
 public class CthughaApplication {
 
---
+++ src/main/java/com/cthugha/filter/UserFilter.java
@@ -0,0 +1,111 @@
+package com.cthugha.filter;
+
+import com.cthugha.util.CookieUtils;
+import com.cthugha.util.JwtUitls;
+import com.google.gson.Gson;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.annotation.Order;
+import org.springframework.util.ObjectUtils;
+import org.springframework.util.StringUtils;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.apache.logging.log4j.util.Strings.isNotBlank;
+
+/**
+ * @Classname UserFilter
+ * @Description 用户验证的过滤器
+ * @Date 2021/12/8 23:25
+ * @Created by gaoqi
+ */
+@WebFilter(filterName = "userFilter", urlPatterns={"/drama"})
+public class UserFilter implements Filter {
+    @Autowired
+    JwtUitls jwtUitls;
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        System.out.println("用户过滤器初始化");
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        System.out.println("过滤器触发");
+        Map<String,String> map = new HashMap<>();
+        String url =  ((HttpServletRequest)servletRequest).getRequestURI();
+        if(url != null){
+            //通过登录请求和静态资源
+            if(canPass(url)){
+                filterChain.doFilter(servletRequest,servletResponse);
+                return;
+            }else{
+                //其他请求验证token
+                //从cookies中获得token
+                String token = CookieUtils.getToken(servletRequest);
+                if(!ObjectUtils.isEmpty(token)){
+                    //token验证结果
+                    int verify  = jwtUitls.verify(token);
+                    if(verify != 1){
+                        //验证失败
+                        if(verify == 2){
+                            map.put("state", "2");
+                            map.put("message","token已过期");
+                        }else if(verify == 0){
+                            map.put("state", "2");
+                            map.put("message","用户信息验证失败");
+                        }
+                    }else if(verify  == 1){
+                        //验证成功，放行
+                        filterChain.doFilter(servletRequest,servletResponse);
+                        return;
+                    }
+                }else{
+                    //token为空的返回
+                    map.put("state", "2");
+                    map.put("message","未携带token信息");
+                }
+            }
+            Gson gson = new Gson();
+            String json = gson.toJson(map);
+            servletResponse.setContentType("application/json");
+            //设置响应的编码
+            servletResponse.setCharacterEncoding("utf-8");
+            //响应
+            PrintWriter pw=servletResponse.getWriter();
+            pw.write(json);
+            pw.flush();
+            pw.close();
+        }
+    }
+
+
+    @Override
+    public void destroy() {
+        System.out.println("用户过滤器销毁");
+    }
+
+    private boolean canPass(String url) {
+        if (url.contains(".css"))
+            return true;
+        if (url.contains("f"))
+            return true;
+        if (url.contains(".js"))
+            return true;
+        if ("/login".equals(url))
+            return true;
+        if ("/account/check".equals(url))
+            return true;
+        if ("/register".equals(url))
+            return true;
+        if ("/".equals(url))
+            return true;
+
+        return false;
+    }
+}
